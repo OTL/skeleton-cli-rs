a0b0e8c92fad43c091d7bc0a3cbf1ac2f9911172
Guillem Nieto <gnieto.talo@gmail.com>
2017-06-12 22:12:44 +0200

    Initialize logger

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+extern crate error_chain;
+extern crate log;
+extern crate env_logger;
+extern crate clap;
+
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init().unwrap();
+
+    if let Err(ref e) = run() {
+        println!("error: {}", e);
+
+        for e in e.iter().skip(1) {
+            println!("caused by: {}", e);
+        }
+
+        if let Some(backtrace) = e.backtrace() {
+            println!("backtrace: {:?}", backtrace);
+        }
+
+        ::std::process::exit(1);
+    }
+}
+
+fn run() -> Result<()> {
+    Ok(())
 }
